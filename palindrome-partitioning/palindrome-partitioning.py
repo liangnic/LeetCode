0e5f9a98e870cb9057b10e3cea9105c38d6ccaaa
Nicole Liang <liangnic@hotmail.com>
2022-01-05 23:32:46 +1100

    Time: 767 ms (27.23%), Space: 27.6 MB (64.19%) - LeetHub

---
+++ palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        if len(s) == 0:
+            return [[]]
+        
+        if len(s) == 1:
+            return [[s]]
+        
+        ans = []
+        
+        for i in range(1, len(s)+1):
+            left = s[:i]
+            right = s[i:]
+            
+            if left == left[::-1]:
+                right = self.partition(right)
+                
+                for j in range(len(right)):
+                    ans.append([left]+right[j])
+        
+        return ans
+        
